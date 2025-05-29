ccd124b51c9ba78c8774a06a90e3b95c0bbde64d
shadrackandare <andareomondi@gmail.com>
2025-05-29 18:05:21 +0300

    added a new fix lemme see if it works

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,11 +18,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider attribute="class" defaultTheme="system">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
         </ThemeProvider>
       </body>
